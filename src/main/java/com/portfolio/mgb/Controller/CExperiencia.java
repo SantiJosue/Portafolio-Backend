2f562429fe08f61dc9b971af243f122f169e0e57
Santiago <santicomercio7@gmail.com>
2023-03-09 16:57:14 -0300

    corrigiendo errores de CORS

--- src/main/java/com/portfolio/mgb/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/mgb/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-78d66.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
