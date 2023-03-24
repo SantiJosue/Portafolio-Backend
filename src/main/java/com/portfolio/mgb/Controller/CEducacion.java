5caab0192285b3aa30028f07da52d39f015af3fb
Santiago <santicomercio7@gmail.com>
2023-03-24 18:33:38 -0300

    terminando HyS

--- src/main/java/com/portfolio/mgb/Controller/CEducacion.java
+++ src/main/java/com/portfolio/mgb/Controller/CEducacion.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 @CrossOrigin(origins = "https://portfolio-b4357.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     @Autowired SEducacion sEducacion;
     
@@ -41,6 +43,7 @@ public class CEducacion {
         return new ResponseEntity(educacion, HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         if (!sEducacion.existsById(id)) {
@@ -50,6 +53,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("producto eliminado"), HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoEducacion dtoeducacion){
         if(StringUtils.isBlank(dtoeducacion.getNombreE())){
@@ -65,6 +69,7 @@ public class CEducacion {
         return new ResponseEntity(new Mensaje("Educacion creada"), HttpStatus.OK);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")    
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoEducacion dtoeducacion){
         if(!sEducacion.existsById(id)){
--- src/main/java/com/portfolio/mgb/Controller/CHyS.java
+++ src/main/java/com/portfolio/mgb/Controller/CHyS.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "https://portfolio-b4357.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/skill")
 public class CHyS {
     @Autowired SHyS shys;
@@ -39,7 +41,8 @@ public class CHyS {
         HyS hYs = shys.getOne(id).get();
         return new ResponseEntity(hYs, HttpStatus.OK);
     }
-
+    
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id) {
         if (!shys.existsById(id)) {
@@ -49,6 +52,7 @@ public class CHyS {
         return new ResponseEntity(new Mensaje("Skill eliminado"), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoHyS dtohys) {
         if (StringUtils.isBlank(dtohys.getNombre())) {
@@ -63,7 +67,8 @@ public class CHyS {
 
         return new ResponseEntity(new Mensaje("Skill agregada"), HttpStatus.OK);
     }
-
+    
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoHyS dtohys) {
         //Validamos si existe el ID
